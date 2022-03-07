07b4902e23fcd91cad308795037ceb21c79ff38d
Sixtus Innocent <sixtusinno@gmail.com>
2022-03-07 16:06:19 +0100

    Cyclic sort

---
+++ problems/cyclic_sort.py
@@ -0,0 +1,12 @@
+def cyclic_sort(nums):
+    i = 0
+
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i = i + 1
+    return nums
+
+print(cyclic_sort([1, 5, 6, 4, 3, 2]))
