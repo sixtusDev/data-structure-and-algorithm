6ce282b67080af12aec675bcbeff1a33e2e7c3ef
Sixtus Innocent <sixtusinno@gmail.com>
2022-03-08 23:17:01 +0100

    Find all missing numbers

--- problems/find_all_missing_numbers.py
+++ problems/find_all_missing_numbers.py
@@ -1,25 +1,16 @@
 def find_all_missing_numbers(nums):
-    nums.sort()
-    non_dup_nums = [nums[0]]
-    missing_nums = []
-
-    for i in range(1, len(nums)):
-        num = nums[i]
-        if non_dup_nums[-1] != num:
-            non_dup_nums.append(num)
-    
-    
+    output = []
     i = 0
-    j = 0
-    print(non_dup_nums)
-    while i < len(non_dup_nums):
-        while non_dup_nums[i] != j + 1:
-            missing_nums.append(j + 1)
-            j = j + 1
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+           nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i = i + 1
 
-        i = i + 1
-        j = j + 1
-    
-    return missing_nums
+    for i in range(len(nums)):
+        if i + 1 != nums[i]:
+            output.append(i + 1)
+    return output
 
-print(find_all_missing_numbers([2, 3, 2, 1]))
+print(find_all_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1]))
