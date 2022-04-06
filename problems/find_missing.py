f426758ed537ae53a38d00e4f1905e48acb26b9c
Sixtus Innocent <sixtusinno@gmail.com>
2022-04-06 14:27:53 +0100

    Find missing number using XOR

--- problems/find_missing.py
+++ problems/find_missing.py
@@ -12,7 +12,20 @@ def find_missing_number1(arr):
 
     return sum
 
+def find_missing_number_usingXOR(arr):
+    n = len(arr) + 1
+    x1 = 1
+    for i in range(2, n + 1):
+        x1 = x1 ^ i
+
+    x2 = arr[0]
+    for i in range(1, len(arr)):
+        x2 = x2 ^ arr[i]
+    
+    return x1 ^ x2
+
 def main():
     print(find_missing_number1([1, 5, 2, 6, 4]))
+    print(find_missing_number_usingXOR([1, 5, 2, 6, 4]))
 
 main()
