7e7bf5987957bbfbc07091f74bd2a9a389948bb6
Sixtus Innocent <sixtusinno@gmail.com>
2022-04-06 14:24:13 +0100

    Modified find max number in bitonic array

--- problems/find_max_in_bitonic.py
+++ problems/find_max_in_bitonic.py
@@ -1,5 +1,6 @@
-from turtle import right
-
+# Find the maximum value in a given Bitonic array. An array is considered bitonic if it is
+# monotonically increasing and then monotonically decreasing. Monotonically increasing or 
+# decreasing means that for any index i in the array arr[i] != arr[i+1]
 
 def find_max_in_bitonic(arr):
     left = 0
@@ -18,5 +19,5 @@ def find_max_in_bitonic(arr):
 def main():
     print(find_max_in_bitonic([1, 3, 8, 12, 4, 2]))
     print(find_max_in_bitonic([3, 8, 3, 1]))
-    
+
 main()
