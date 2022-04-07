29f186b7317297234b9093b5aa6307f7d7932379
Sixtus Innocent <sixtusinno@gmail.com>
2022-04-07 18:11:21 +0100

    Check permutation using hash map

---
+++ cracking-the-coding-interview-solutions/check_permutation.py
@@ -0,0 +1,29 @@
+# Given two strings, write a method to decide if one is a permutation of the other.
+
+def check_permutation_using_hash_map(stringA, stringB):
+    if len(stringA) != len(stringB):
+        return False
+
+    char_ocurrence = {}
+    for char in stringA:
+        if char in char_ocurrence:
+            char_ocurrence[char] += 1
+        else:
+            char_ocurrence[char] = 1
+
+    for char in stringB:
+        if char not in char_ocurrence:
+            return False
+        if char in char_ocurrence:
+            if char_ocurrence[char] == 1:
+                del char_ocurrence[char]
+            else:
+                char_ocurrence[char] -= 1
+    
+    return False if len(char_ocurrence) > 0 else True
+
+
+def main():
+    print(check_permutation_using_hash_map("abc", "bca"))
+
+main()
